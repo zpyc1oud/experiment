74e429e71aa72fd48313bc1fbb251e4d44ef3355
Andy-Huang-SZU <Andysfls0808@163.com>
2025-11-20 13:59:33 +0800

    Add a for loop

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "Loop " << i + 1 << std::endl;
+    }
+    return 0;
+}
