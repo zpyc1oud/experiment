05c8c5d41bc68accd8cec04aadc1edb3c9a0c5c5
bluesun <2735216900@qq.com>
2025-11-26 16:52:18 +0800

    加了change的修改

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    for (int i = 0; i < 5; ++i) {
+        std::cout <<"change"<< "Loop " << i + 1 << std::endl;
+    }
+    // ljy 2023040431 11_26修改
+    return 0;
+}
